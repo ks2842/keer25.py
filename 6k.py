af6392f21f3c752188469a11ed596ae04da87e90
ks2842 <51949666+ks2842@users.noreply.github.com>
2019-07-03 16:58:58 +0530

    Update 6k.py

--- 6k.py
+++ 6k.py
@@ -1,7 +1,7 @@
 lp,lk=map(str,input().split())
 if(len(lp)!=len(lp)):
   print("no")
-c1=[ld.count(i) for i in lp]
+c1=[lp.count(i) for i in lp]
 c2=[lk.count(i) for i in lk]
 if c1==c2:
   print("yes")
